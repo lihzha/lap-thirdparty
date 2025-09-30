fc89e1977df1a0a1164dd1094ff7f2813fed0ec9
Lihan Zha <lihanzha20@gmail.com>
2025-09-30 10:34:55 -0400

    fix

--- src/openpi_cot/dataloader/cot_rlds_dataset.py
+++ src/openpi_cot/dataloader/cot_rlds_dataset.py
@@ -976,11 +976,12 @@ class _DroidCoTRldsDatasetRaw(_SingleCoTRldsDatasetRaw):
         self.control_frequency: int = 15
 
         if train_dataset is not None:
-            # self.lang_table = train_dataset.lang_table
+            if self.use_base_actions:
+                self.lang_table = train_dataset.lang_table
+                self.cam_table = train_dataset.cam_table
+                self.intr_table = train_dataset.intr_table
+                self.extr_table = train_dataset.extr_table
             self.ep_table = train_dataset.ep_table
-            # self.cam_table = train_dataset.cam_table
-            # self.intr_table = train_dataset.intr_table
-            # self.extr_table = train_dataset.extr_table
             self.instr_table = train_dataset.instr_table
             self.filter_table = train_dataset.filter_table
 
@@ -996,11 +997,12 @@ class _DroidCoTRldsDatasetRaw(_SingleCoTRldsDatasetRaw):
             else:
                 raise ValueError(f"Unknown language action directory: {language_action_dir}")
 
-            # self.lang_table = self.build_lang_action_table(language_action_dir)
+            if self.use_base_actions:
+                self.lang_table = self.build_lang_action_table(language_action_dir)
+                self.cam_table, self.intr_table, self.extr_table = self.build_cam_tables(
+                    metadata_path, need_calib=self.need_calib
+                )
             self.ep_table = self.build_lookup_table(metadata_path)
-            # self.cam_table, self.intr_table, self.extr_table = self.build_cam_tables(
-            #     metadata_path, need_calib=self.need_calib
-            # )
             self.instr_table = self.build_instr_table(metadata_path)
             self.filter_table = self.build_filter_table(metadata_path, use_idle_filter=self.use_idle_filter)
 
