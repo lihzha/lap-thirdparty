a617be32eda818fb38f704d1db0ff5a3f4c7ce41
Lihan Zha <lihanzha20@gmail.com>
2025-09-30 20:42:41 -0400

    fix

--- src/openpi_cot/dataloader/oxe_utils/mixtures.py
+++ src/openpi_cot/dataloader/oxe_utils/mixtures.py
@@ -88,8 +88,8 @@ OXE_NAMED_MIXTURES: dict[str, list[tuple[str, float]]] = {
         # # ("utaustin_mutex", 1.0),
         # ("berkeley_fanuc_manipulation", 2.0),
         # ("cmu_stretch", 1.0),
-        # ("bc_z", 0.2),
-        ("fmb", 1.0),
+        ("bc_z", 0.2),
+        # ("fmb", 1.0),
         # # ("dobbe", 0.2),  # question
     ],
 }
