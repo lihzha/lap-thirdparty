e44e8d34910dd5ace34da20fcf9a8ba656e92db2
Lihan Zha <lihanzha20@gmail.com>
2025-09-30 20:30:44 -0400

    fix

--- src/openpi_cot/dataloader/oxe_utils/transforms.py
+++ src/openpi_cot/dataloader/oxe_utils/transforms.py
@@ -798,7 +798,11 @@ def fmb_dataset_transform(trajectory: dict[str, Any]) -> dict[str, Any]:
         ),
         axis=-1,
     )
-    return trajectory
+    movement_actions = trajectory["observation"]["proprio"][1:, :6] - trajectory["observation"]["proprio"][:-1, :6]
+    traj_truncated = tf.nest.map_structure(lambda x: x[:-1], trajectory)
+    traj_truncated["action"] = tf.concat([movement_actions, trajectory["action"][:-1, -1:]], axis=1)
+
+    return traj_truncated
 
 
 def dobbe_dataset_transform(trajectory: dict[str, Any]) -> dict[str, Any]:
