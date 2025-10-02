66baf72a59cc384fb5df77960486a767cc5c269e
Lihan Zha <lihanzha20@gmail.com>
2025-10-02 15:28:41 -0400

    fix

--- src/openpi_cot/models/pi_cot.py
+++ src/openpi_cot/models/pi_cot.py
@@ -238,19 +238,21 @@ class PiCoT(_pi0.Pi0):
         observation: CoTObservation,
         *,
         num_steps: int | at.Int[at.Array, ""] = 10,
+        noise: at.Float[at.Array, "b ah ad"] | None = None,
     ) -> _model.Actions:
         # 1) Sample reasoning tokens (left-padded already) and build KV cache
-        prefix_mask, kv_cache, _, _ = self._sample_reasoning_tokens(observation)
+        prefix_mask, _, prefix_tokens, _, t, k_cache, v_cache = self._sample_reasoning_tokens(observation)
 
         # note that we use the convention more common in diffusion literature, where t=1 is noise and t=0 is the target
         # distribution. yes, this is the opposite of the pi0 paper, and I'm sorry.
         dt = -1.0 / num_steps
         batch_size = observation.state.shape[0]
-        noise = jax.random.normal(rng, (batch_size, self.action_horizon, self.action_dim))
+        if noise is None:
+            noise = jax.random.normal(rng, (batch_size, self.action_horizon, self.action_dim))
 
         def step(carry):
             x_t, time = carry
-            suffix_tokens, suffix_mask, suffix_ar_mask = self.embed_suffix(
+            suffix_tokens, suffix_mask, suffix_ar_mask, adarms_cond = self.embed_suffix(
                 observation, x_t, jnp.broadcast_to(time, batch_size)
             )
             # `suffix_attn_mask` is shape (b, suffix_len, suffix_len) indicating how the suffix tokens can attend to each
@@ -262,6 +264,11 @@ class PiCoT(_pi0.Pi0):
             # `combined_mask` is shape (b, suffix_len, prefix_len + suffix_len) indicating how the suffix tokens (which
             # generate the queries) can attend to the full prefix + suffix sequence (which generates the keys and values)
             full_attn_mask = jnp.concatenate([prefix_attn_mask, suffix_attn_mask], axis=-1)
+            assert full_attn_mask.shape == (
+                batch_size,
+                suffix_tokens.shape[1],
+                prefix_tokens.shape[1] + suffix_tokens.shape[1],
+            )
             # `positions` is shape (b, suffix_len) indicating the positions of the suffix tokens
             positions = jnp.sum(prefix_mask, axis=-1)[:, None] + jnp.cumsum(suffix_mask, axis=-1) - 1
 
@@ -269,7 +276,8 @@ class PiCoT(_pi0.Pi0):
                 [None, suffix_tokens],
                 mask=full_attn_mask,
                 positions=positions,
-                kv_cache=kv_cache,
+                kv_cache=(k_cache, v_cache),
+                adarms_cond=[None, adarms_cond],
             )
             assert prefix_out is None
             v_t = self.action_out_proj(suffix_out[:, -self.action_horizon :])
@@ -277,7 +285,7 @@ class PiCoT(_pi0.Pi0):
             return x_t + dt * v_t, time + dt
 
         def cond(carry):
-            _x_t, time = carry
+            x_t, time = carry
             # robust to floating-point error
             return time >= -dt / 2
 
