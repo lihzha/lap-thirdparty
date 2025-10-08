7d3ddc9ce1cea083cec1ddfe9e391568aa0baeda
Lihan Zha <lihanzha20@gmail.com>
2025-10-08 19:31:37 -0400

    add

--- src/openpi_cot/dataloader/oxe_utils/mixtures.py
+++ src/openpi_cot/dataloader/oxe_utils/mixtures.py
@@ -65,7 +65,7 @@ OXE_NAMED_MIXTURES: dict[str, list[tuple[str, float]]] = {
     #     ("roboturk", 2.0),
     # ],
     "oxe_pi_magic_soup": [
-        ("droid", 0.5),
+        # ("droid", 0.5),
         # # ("bc_z", 0.5),
         # ("fractal20220817_data", 1.0),
         # # # ("kuka", 0.1),
@@ -93,7 +93,7 @@ OXE_NAMED_MIXTURES: dict[str, list[tuple[str, float]]] = {
         # ("fmb", 1.0),
         # ("dobbe", 0.2),  # question
         # ("sample_r1_lite", 0.2),  # only joint position available for both actions and states
-        # ("agibot_dataset", 0.2),
+        ("agibot_dataset", 0.2),
     ],
 }
 
