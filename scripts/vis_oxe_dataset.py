d13fe1240c5e134cfcd49e250f57f99fab36655b
Lihan Zha <lihanzha20@gmail.com>
2025-10-14 10:40:04 -0400

    add

--- scripts/vis_oxe_dataset.py
+++ scripts/vis_oxe_dataset.py
@@ -210,7 +210,7 @@ def _draw_text_block(img: np.ndarray, text: str, area: tuple[int, int, int, int]
     block_h = max(1, y1 - y0)
     base_scale = 2.5
     scale = max(0.4, min(1.5, block_h / 110.0)) * base_scale
-    font_size = int(13 * scale * 0.5)
+    font_size = int(13 * scale * 0.6)
 
     try:
         font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", font_size)
