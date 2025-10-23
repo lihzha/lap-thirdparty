6243970c4946b94e0bac92c44693cae106f7076f
lihzha <lihanzha20@gmail.com>
2025-10-23 16:22:19 -0400

    remove breakpoint

--- scripts/train.py
+++ scripts/train.py
@@ -856,7 +856,6 @@ def main(config: _config.TrainConfig):
     data_iter = iter(data_loader)
     log_mem("Before getting batch")
     batch = next(data_iter)
-    breakpoint()
     vis_batch(batch, tok=tok)
 
     log_mem("After getting batch")
