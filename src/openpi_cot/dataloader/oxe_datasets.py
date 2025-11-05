0897c9a3ed6dc98a3f4b2eec066be6c9cc87b3ab
lihzha <lihanzha20@gmail.com>
2025-11-05 13:31:47 -0500

    libero fix

--- src/openpi_cot/dataloader/oxe_datasets.py
+++ src/openpi_cot/dataloader/oxe_datasets.py
@@ -392,32 +392,6 @@ class LiberoCoTDataset(_SingleOXECoTDataset):
 
         self.dataset = self.dataset.traj_map(_pop_and_rename_keys, self.num_parallel_calls)
 
-    def apply_traj_filters(self, action_key):
-        # def is_nonzero_length(traj):
-        #     return tf.shape(traj[action_key])[0] > 0
-
-        # def has_any_instruction(traj):
-        #     instr = traj["language_instruction"]
-        #     instr = tf.reshape(instr, [-1])
-        #     instr = tf.strings.strip(instr)
-        #     return tf.reduce_any(tf.strings.length(instr) > 0)
-
-        # self.dataset = self.dataset.filter(has_any_instruction)
-
-        # self.dataset = self.dataset.filter(is_nonzero_length)
-        pass
-
-    def apply_frame_filters(self):
-        """
-        Optionally applied *per-dataset* transforms that happen at a frame level.
-        """
-
-        # # Always drop frames with empty/whitespace-only prompts
-        # def _non_empty_prompt(frame: dict) -> tf.Tensor:
-        #     p = tf.strings.strip(frame["prompt"])  # scalar tf.string after flatten
-        #     return tf.strings.length(p) > 0
-
-        # self.dataset = self.dataset.filter(_non_empty_prompt)
 
 
 class SampleR1LiteCoTDataset(_SingleOXECoTDataset):
