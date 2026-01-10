119401f426907913941622dd52348b6e7a142ce9
lihzha <lihanzha20@gmail.com>
2026-01-10 12:51:22 -0500

    add new vqa mixture

--- src/openpi_cot/datasets/utils/mixtures.py
+++ src/openpi_cot/datasets/utils/mixtures.py
@@ -108,6 +108,44 @@ OXE_NAMED_MIXTURES: dict[str, list[tuple[str, float]]] = {
         ("paco_lvis", 1.0),
         ("paco_ego4d", 1.0),
     ],
+    "oxe_magic_soup_vqa01": [
+        # ("kuka", 0.01),  # no language instructions, 580392 trajs, no wrist, action scale is strange
+        ("bc_z", 0.05),
+        ("droid", 2.0),
+        ("fractal20220817_data", 1.0),
+        ("bridge_v2_oxe", 1.0),
+        ("taco_play", 2.0),
+        (
+            "jaco_play",
+            1.0,
+        ),  # gripper state and action still seems incorrect. Action sometimes should be 1 but is 0. State seems random. Ignore for now.
+        ("furniture_bench_dataset_converted_externally_to_rlds", 0.05),
+        ("utaustin_mutex", 1.0),
+        ("berkeley_fanuc_manipulation", 2.0),  # not sure quaternion is xyzw or wxyz
+        # (
+        #     "cmu_stretch",
+        #     2.0,
+        # ),  # almost all movements are "move up". gripper actions always zero, use gripper state as action.
+        ("fmb", 0.05),
+        # ("dobbe", 0.2),  # question
+        ("berkeley_autolab_ur5", 1.0),
+        # ("dlr_edan_shared_control_converted_externally_to_rlds", 1.0),  # has empty language instructions, euler is zxy
+        # ("roboturk", 2.0),  # no prio, 2144 trajs. delta actions slightly sketchy sometimes. loss to high.
+        ### To be tested
+        ("austin_buds_dataset_converted_externally_to_rlds", 1.0),  # no language instructions, 50 trajs
+        ("austin_sailor_dataset_converted_externally_to_rlds", 1.0),  # no language instructions, 250 trajs
+        ("austin_sirius_dataset_converted_externally_to_rlds", 1.0),  # no language instructions, 600 trajs
+        ("viola", 1.0),  # gripper mostly out of view, 135 trajs
+        ("molmoact_dataset", 1.0),
+        ("molmoact_dataset", 1.0),
+        ("coco_captions", 0.25),  # 10% COCO caption samples
+        ("vqa", 0.025),  # 10% vqa samples
+        ("lvis", 0.0025),
+        ("pixmo_cap", 0.25),  # 10% COCO caption samples
+        ("pixmo_point", 0.025),  # 10% vqa samples
+        ("paco_lvis", 0.25),
+        ("paco_ego4d", 0.25),
+    ],
     "franka_no_droid": [
         ("furniture_bench_dataset_converted_externally_to_rlds", 0.5),
         ("utaustin_mutex", 1.0),
