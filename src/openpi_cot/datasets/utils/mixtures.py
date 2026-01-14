2ce4440740be22b75a391564ba5e8c748754111e
lihzha <lihanzha20@gmail.com>
2026-01-13 23:44:18 -0500

    test mixture

--- src/openpi_cot/datasets/utils/mixtures.py
+++ src/openpi_cot/datasets/utils/mixtures.py
@@ -245,5 +245,6 @@ OXE_NAMED_MIXTURES: dict[str, list[tuple[str, float]]] = {
     "paco_ego4d": [("paco_ego4d", 1.0)],
     "droid_bbox": [("droid_bbox", 1.0)],
     "droid_100": [("droid_100", 1.0)],
+    "test_mixture": [("droid_bbox", 3.0), ("fmb", 1.0)]
 }
 ## to use ut_austin_mutex, we need to flip_wrist_image_channels,flip_image_channels. Other datasets are fine.
