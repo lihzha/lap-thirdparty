1b9863c82f2e3ad58b6a8424583357a6b45bfc19
lihzha <lihanzha20@gmail.com>
2026-01-15 21:25:37 -0500

    fix mixture

--- src/openpi_cot/datasets/utils/mixtures.py
+++ src/openpi_cot/datasets/utils/mixtures.py
@@ -181,9 +181,9 @@ OXE_NAMED_MIXTURES: dict[str, list[tuple[str, float]]] = {
         ("pixmo_point", 1.0),  # 10% vqa samples
         ("paco_lvis", 10.0),
         ("paco_ego4d", 20.0),
-        ("droid_bbox", 20.0),
-        ("molmoact_bbox", 20.0),
-        # ("bridge_bbox", 10.0),
+        ("droid_bbox", 10.0),
+        ("molmoact_bbox", 10.0),
+        ("bridge_bbox", 10.0),
     ],
     "franka_no_droid": [
         ("furniture_bench_dataset_converted_externally_to_rlds", 0.5),
