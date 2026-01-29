6bf1072373742ba1e124454925b0235038e5ce7f
JM <jmattarian@cs.toronto.edu>
2026-01-29 12:15:02 -0600

    Switch to matrices for both arms

--- scripts/real_robot/aloha_main.py
+++ scripts/real_robot/aloha_main.py
@@ -1,8 +1,10 @@
 # ruff: noqa
 import numpy as np
 import collections
+from openpi.training import config
 from openpi_client import image_tools
 from aloha.real_env import RealEnv
+from aloha.robot_utils import move_arms, START_ARM_POSE
 import interbotix_common_modules.angle_manipulation as ang
 from aloha.robot_utils import move_grippers, load_yaml_file # requires aloha
 from interbotix_common_modules.common_robot.robot import (
@@ -46,12 +48,14 @@ class RealEnvCartesian(RealEnv):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        right_arm_init_R_T = np.array([[9.98300963e-01, 1.27577457e-03,-5.82542630e-02, 2.55826044e-01], [-1.09735611e-03, 9.99994609e-01, 3.09464090e-03, -3.15635386e-04], [5.82578970e-02, -3.02545732e-03, 9.98296982e-01, 2.88863282e-01], [0.00000000e+00,0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
         left_arm_init_R_T = np.array([[0.87965611, 0.02124257, 0.47513565, 0.33629917], [-0.01259074, 0.99969204, -0.02138442, -0.00444487], [-0.47544359, 0.01282863, 0.87965267, 0.2516704], [0., 0., 0., 1.]])
-        self.follower_bots[1].arm.set_ee_pose_matrix(left_arm_init_R_T, moving_time=20.0, accel_time=1.0, blocking=True)
+        self.follower_bots[0].arm.set_ee_pose_matrix(right_arm_init_R_T, moving_time=10.0, accel_time=1.0, blocking=True)
+        self.follower_bots[1].arm.set_ee_pose_matrix(left_arm_init_R_T, moving_time=10.0, accel_time=1.0, blocking=True)
 
     def set_relative_ee(self, dx=0, dy=0, dz=0, droll=0, dpitch=0, dyaw=0, custom_guess=None, execute=True, moving_time=None, accel_time=None, blocking=True):
         
-        R_T_curr = self.follower_bots[0].arm.T_sb
+        R_T_curr = self.follower_bots[0].arm.get_ee_pose()
         R_curr = R_T_curr[:3, :3]
         R_delta = ang.eulerAnglesToRotationMatrix([droll, dpitch, dyaw])
 
@@ -104,7 +108,7 @@ class RealEnvCartesian(RealEnv):
 
         breakpoint()
 
-        # _, success = self.set_relative_ee(dx=dx, dy=dy, dz=dz, droll=droll, dpitch=dpitch, dyaw=dyaw, moving_time=20.0, accel_time=0.1, blocking=True)
+        # _, success = self.set_relative_ee(dx=dx, dy=dy, dz=dz, droll=droll, dpitch=dpitch, dyaw=dyaw, moving_time=10.0, accel_time=0.1, blocking=True)
         if get_obs:
             obs = self.get_observation(get_base_vel)
         else:
@@ -156,7 +160,9 @@ class AlohaEvalRunner(BaseEvalRunner):
         # the object is on the left, while in the wrist image, the object is on the right, 
         # then you should rotate the wrist image by 180 degrees, i.e. wrist_image[::-1, ::-1, ::-1]
 
-        right_image = right_image.transpose[::-1, ::-1, :]
+        left_image = left_image[:, :, ::-1]
+        right_image = right_image[::-1, ::-1, ::-1]
+        breakpoint()
  
         if save_to_disk:
             combined_image = np.concatenate([left_image, right_image], axis=1)
@@ -179,7 +185,6 @@ class AlohaEvalRunner(BaseEvalRunner):
             "state": np.concatenate([cartesian_position, gripper_position]),
             "euler": euler,
         }
-
 
 if __name__ == "__main__":
     args: Args = tyro.cli(Args)
